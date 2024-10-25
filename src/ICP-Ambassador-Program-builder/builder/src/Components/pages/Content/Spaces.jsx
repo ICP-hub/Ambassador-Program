50cb3a5d0cbbfc54bc42ad62e3205160ee4bfd1f
neerajaquadbcompany <neeraja.naru@quadbtech.com>
2024-10-25 08:50:46 +0000

    modified button

--- src/ICP-Ambassador-Program-builder/builder/src/Components/pages/Content/Spaces.jsx
+++ src/ICP-Ambassador-Program-builder/builder/src/Components/pages/Content/Spaces.jsx
@@ -7,13 +7,20 @@ import Select from '@mui/material/Select';
 
 const Spaces = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
   const [chain, setchain] = useState('');
 
   const handleChange = (event) => {
     setchain(event.target.value);
   };
+
+  const handleClick = () => {
+    setIsClicked(true);
+    handleModalToggle();
+  };
  
   const handleModalToggle = () => {
+   
     setIsModalOpen(!isModalOpen);
   };
 
@@ -97,9 +104,11 @@ const Spaces = () => {
 
             {/* Buttons */}
             <div className='flex justify-end space-x-2 mt-6'>
-              <button
-                className='bg-gray-300 text-black py-2 px-4 rounded-md'
-                onClick={handleModalToggle}
+            <button
+                className={`${
+                  isClicked ? 'bg-blue-200' : 'bg-transparent'
+                } text-blue-500 py-2 px-4 rounded-md transition-colors duration-300`}
+                onClick={handleClick}
               >
                 Cancel
               </button>
