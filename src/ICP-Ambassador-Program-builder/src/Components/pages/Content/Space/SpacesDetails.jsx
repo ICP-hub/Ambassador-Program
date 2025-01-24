443eab67962587395be7777f19c5a25595cd6f96
avanishsrivastava2907 <avanish.ranjan@quadbtech.com>
2025-01-24 15:32:41 +0530

    access_control

--- src/ICP-Ambassador-Program-builder/src/Components/pages/Content/Space/SpacesDetails.jsx
+++ src/ICP-Ambassador-Program-builder/src/Components/pages/Content/Space/SpacesDetails.jsx
@@ -1,81 +1,136 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Navbar from '../../Navbar/Navbar';
-import Footer from '../../Footer/Footer';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Avatar,Button, IconButton, Box, Menu, MenuItem } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { updateMission } from '../../../../redux/mission/missionSlice';
-import toast from 'react-hot-toast';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../../Navbar/Navbar";
+import Footer from "../../Footer/Footer";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Avatar,
+  Button,
+  IconButton,
+  Box,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { updateMission } from "../../../../redux/mission/missionSlice";
+import toast from "react-hot-toast";
 
-import TaskIcon from '@mui/icons-material/Task';
-const SpacesDetails = ({setLoading}) => {
-    const navigate=useNavigate();
-  const [statusFilter, setStatusFilter] = useState('all');
+import TaskIcon from "@mui/icons-material/Task";
+import { useAuthClient } from "../../../../utils/useAuthClient";
+import { Principal } from "@dfinity/principal";
+const SpacesDetails = ({ setLoading }) => {
+  const navigate = useNavigate();
+  const [statusFilter, setStatusFilter] = useState("all");
   const [expireFilter, setExpireFilter] = useState(true);
+
   const [anchorEl, setAnchorEl] = useState(null);
-  const spaces=useSelector(state=>state.spaces.value)
-  const actors=useSelector(state=>state.actor.value)
-  const [missionList,setMissionList]=useState([])
-  const [missionArr,setMissionArr]=useState([])
-  const dispatch=useDispatch()
-  
+  const spaces = useSelector((state) => state.spaces.value);
+  const actors = useSelector((state) => state.actor.value);
+  const [missionList, setMissionList] = useState([]);
+  const [missionArr, setMissionArr] = useState([]);
+  const [owner, setOwner] = useState(false);
+  const { principal } = useAuthClient();
 
-  async function fetchMissions(){
+  const [moderator, setModerator] = useState(false);
+  const [editor, setEditor] = useState(false);
+  const dispatch = useDispatch();
+
+  async function fetchMissions() {
     try {
-      const res=await actors?.backendActor.get_all_space_missions(spaces?.space_id)
-      console.log("fetch mission response : ",res,res?.Ok?.length)
-      if(res!=undefined && res!=null && res?.Ok!=undefined){
-        setMissionArr(res?.Ok)
-        console.log(new Array(res?.Ok?.length).fill({ id: 1, status: 'draft', expired: '---' }))
+      const res = await actors?.backendActor.get_all_space_missions(
+        spaces?.space_id
+      );
+      console.log("fetch mission response : ", res, res?.Ok?.length);
+      if (res != undefined && res != null && res?.Ok != undefined) {
+        setMissionArr(res?.Ok);
+        console.log(
+          new Array(res?.Ok?.length).fill({
+            id: 1,
+            status: "draft",
+            expired: "---",
+          })
+        );
         // setMissionList(new Array(res?.Ok).fill({ id: 1, status: 'draft', expired: '---' }))
-        setMissionList(res?.Ok)
+        setMissionList(res?.Ok);
       }
     } catch (error) {
-      console.log("error fetching missions : ",error)
+      console.log("error fetching missions : ", error);
     }
   }
 
-  async function createDraftMission(){
+  async function createDraftMission() {
     try {
-      setLoading(true)
-      const res=await actors?.backendActor.create_draft_mission(spaces.space_id)
-      console.log(res)
-      if(res?.Ok==null && res!=undefined && res!=null){
-        toast.success("Mission added as a draft successfully")
-        window.location.reload()
-      }else{
-        toast.error('Some error occurred!')
-        setLoading(false)
+      setLoading(true);
+      const res = await actors?.backendActor.create_draft_mission(
+        spaces.space_id
+      );
+      console.log(res);
+      if (res?.Ok == null && res != undefined && res != null) {
+        toast.success("Mission added as a draft successfully");
+        window.location.reload();
+      } else {
+        toast.error("Some error occurred!");
+        setLoading(false);
       }
     } catch (error) {
-      setLoading(false)
-      toast.error("Something went wrong")
-      console.log("error creating draft mission : ",error)
+      setLoading(false);
+      toast.error("Something went wrong");
+      console.log("error creating draft mission : ", error);
     }
   }
 
-  useEffect(()=>{
-    if(spaces.space_id==undefined){
-      navigate('/')
+  useEffect(() => {
+    if (spaces.space_id == undefined) {
+      navigate("/");
     }
-    fetchMissions()
-  },[actors])
+    fetchMissions();
+  }, [actors]);
+
+  useEffect(() => {
+    if (spaces && spaces.moderators && spaces.editors) {
+      if (principal) {
+        const isModerator = spaces.moderators.some(
+          (mod) => mod.toText() === principal.toText()
+        );
+
+        const isEditor = spaces.editors.some(
+          (edit) => edit.toText() === principal.toText()
+        );
+
+        const isOwner = spaces.owner.toText() === principal.toText();
+        console.log("isOwner : ", isOwner);
+        console.log("isModerator : ", isModerator);
+        console.log(isEditor, "isEditor");
+
+        // Update states
+        setOwner(isOwner);
+        setModerator(isModerator || isOwner);
+        setEditor(isEditor || isOwner);
+      }
+    }
+  }, [spaces, principal]);
 
   const rows = [
-    { id: 1, status: 'draft', expired: '---' },
-    { id: 2, status: 'draft', expired: '---' },
-    { id: 3, status: 'draft', expired: '---' },
-    { id: 4, status: 'draft', expired: '---' },
-    { id: 5, status: 'draft', expired: '---' },
+    { id: 1, status: "draft", expired: "---" },
+    { id: 2, status: "draft", expired: "---" },
+    { id: 3, status: "draft", expired: "---" },
+    { id: 4, status: "draft", expired: "---" },
+    { id: 5, status: "draft", expired: "---" },
   ];
 
   const toggleStatusFilter = () => {
-    setStatusFilter((prevStatus) => (prevStatus === 'all' ? 'active' : 'all'));
+    setStatusFilter((prevStatus) => (prevStatus === "all" ? "active" : "all"));
   };
 
   const toggleExpireFilter = () => {
@@ -90,45 +145,78 @@ const SpacesDetails = ({setLoading}) => {
     setAnchorEl(null);
   };
 
-  const handleEdit = () =>{
-    navigate('/space_details')
-  }
+  const handleEdit = () => {
+    navigate("/space_details");
+  };
 
-  const handleRole = () =>{
-    navigate('/slug_url/role')
-  }
+  const handleRole = () => {
+    navigate("/slug_url/role");
+  };
 
-  const handleBalance = () =>{
-    navigate('/slug_url/balance')
-  }
+  const handleBalance = () => {
+    navigate("/slug_url/balance");
+  };
 
-  const handleMission = (index) =>{
-    dispatch(updateMission(missionArr[index]))
-    navigate('/slug_url/mission/223/edit')
-  }
+  const handleMission = (index) => {
+    dispatch(updateMission(missionArr[index]));
+    navigate("/slug_url/mission/223/edit");
+  };
 
-  const handleTasks = (row) =>{
-    navigate('/mission/223/tasks',{ state: { row } });
-  }
+  const handleTasks = (row) => {
+    navigate("/mission/223/tasks", { state: { row } });
+  };
 
-  const filteredRows = statusFilter === 'all' ? rows : rows.filter(row => row.status === 'active');
+  const filteredRows =
+    statusFilter === "all"
+      ? rows
+      : rows.filter((row) => row.status === "active");
   // const fil
 
   return (
-    <div className=''>
-      <Navbar nav={navigate}/>
-      <div className='py-3 px-5 lg:px-20 h-screen'>
-        <div className='flex justify-between items-center'>
-          <div className='flex gap-3 items-center '>
-            <div className='font-semibold text-md mr-8'>Space name : {spaces?.name}</div>
-            <div className='text-sm text-white bg-black hover:bg-blue-700 py-2 px-4 rounded cursor-pointer shadow-2xl' onClick={handleEdit}>EDIT</div>
-            <div className='text-sm text-white bg-black py-2 hover:bg-blue-700 px-4 rounded cursor-pointer shadow-2xl' onClick={handleRole}>ROLES</div>
-            <div className='text-sm text-white bg-black py-2 hover:bg-blue-700 px-4 rounded cursor-pointer shadow-2xl'onClick={handleBalance}>BALANCE</div>
+    <div className="">
+      <Navbar nav={navigate} />
+      <div className="py-3 px-5 lg:px-20 h-screen">
+        <div className="flex justify-between items-center">
+          <div className="flex gap-3 items-center ">
+            <div className="font-semibold text-md mr-8">
+              Space name : {spaces?.name}
+            </div>
+            {moderator && (
+              <div
+                className="text-sm text-white bg-black hover:bg-blue-700 py-2 px-4 rounded cursor-pointer shadow-2xl"
+                onClick={handleEdit}
+              >
+                EDIT
+              </div>
+            )}
+            {owner && (
+              <>
+                <div
+                  className="text-sm text-white bg-black py-2 hover:bg-blue-700 px-4 rounded cursor-pointer shadow-2xl"
+                  onClick={handleRole}
+                >
+                  ROLES
+                </div>
+                <div
+                  className="text-sm text-white bg-black py-2 hover:bg-blue-700 px-4 rounded cursor-pointer shadow-2xl"
+                  onClick={handleBalance}
+                >
+                  BALANCE
+                </div>
+              </>
+            )}
           </div>
-          <div className='text-sm text-white bg-black py-2 px-2 lg:px-6 rounded shadow-2xl cursor-pointer' onClick={createDraftMission}>CREATE MISSION</div>
+          {editor && (
+            <div
+              className="text-sm text-white bg-black py-2 px-2 lg:px-6 rounded shadow-2xl cursor-pointer"
+              onClick={createDraftMission}
+            >
+              CREATE MISSION
+            </div>
+          )}
         </div>
 
-        <TableContainer component={Paper} className='mt-4'>
+        <TableContainer component={Paper} className="mt-4">
           <Table>
             <TableHead>
               <TableRow>
@@ -137,22 +225,50 @@ const SpacesDetails = ({setLoading}) => {
                 <TableCell align="center">Reward</TableCell>
                 <TableCell align="center">Pool</TableCell>
                 <TableCell align="center">Token</TableCell>
-                <TableCell align="center" style={{ cursor: 'pointer', width: '150px' }} onClick={toggleStatusFilter}>
-                  <Box display="flex" alignItems="center" justifyContent="center">
+                <TableCell
+                  align="center"
+                  style={{ cursor: "pointer", width: "150px" }}
+                  onClick={toggleStatusFilter}
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
                     Status ({statusFilter})
-                    {statusFilter === 'all' ? <ArrowDropDownIcon fontSize="small" /> : <ArrowDropUpIcon fontSize="small" />}
+                    {statusFilter === "all" ? (
+                      <ArrowDropDownIcon fontSize="small" />
+                    ) : (
+                      <ArrowDropUpIcon fontSize="small" />
+                    )}
                   </Box>
                 </TableCell>
-                <TableCell align="center" style={{ cursor: 'pointer', width: '150px' }} onClick={toggleExpireFilter}>
-                  <Box display="flex" alignItems="center" justifyContent="center">
-                    <div style={{
-                        cursor: 'pointer',
-                        transition: 'background-color 0.3s, transform 0.2s',
-                      }}>Expired</div>
-                    {expireFilter ? <ArrowDropDownIcon fontSize="small" /> : <ArrowDropUpIcon fontSize="small" />}
+                <TableCell
+                  align="center"
+                  style={{ cursor: "pointer", width: "150px" }}
+                  onClick={toggleExpireFilter}
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <div
+                      style={{
+                        cursor: "pointer",
+                        transition: "background-color 0.3s, transform 0.2s",
+                      }}
+                    >
+                      Expired
+                    </div>
+                    {expireFilter ? (
+                      <ArrowDropDownIcon fontSize="small" />
+                    ) : (
+                      <ArrowDropUpIcon fontSize="small" />
+                    )}
                   </Box>
                 </TableCell>
-                <TableCell align="center" style={{ cursor: 'pointer' }}>
+                <TableCell align="center" style={{ cursor: "pointer" }}>
                   <IconButton onClick={handleMenuClick}>
                     <FilterListIcon />
                   </IconButton>
@@ -161,8 +277,12 @@ const SpacesDetails = ({setLoading}) => {
                     open={Boolean(anchorEl)}
                     onClose={handleMenuClose}
                   >
-                    <MenuItem onClick={handleMenuClose}>Recently deployed</MenuItem>
-                    <MenuItem onClick={handleMenuClose}>Time remaining</MenuItem>
+                    <MenuItem onClick={handleMenuClose}>
+                      Recently deployed
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose}>
+                      Time remaining
+                    </MenuItem>
                     <MenuItem onClick={handleMenuClose}>Pending tasks</MenuItem>
                     <MenuItem onClick={handleMenuClose}>Profitability</MenuItem>
                   </Menu>
@@ -173,48 +293,75 @@ const SpacesDetails = ({setLoading}) => {
               </TableRow>
             </TableHead>
 
-            <TableBody >
+            <TableBody>
               {missionList.length > 0 ? (
-                
-                missionList.map((row,index) => {
-                  console.log("filter : ",row)
-                  return(
-                  <TableRow key={index} className='hover:bg-blue-100 cursor-pointer '>
-                  <TableCell align="center" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    {
-                      row?.img?.length>0?
-                    <img src={row?.img[0]} className='w-[40px] h-[40px] rounded-sm object-fill' onClick={()=>handleMission(index)}/>
-
-                      :
-                    <Avatar variant="rounded" sx={{ width: 40, height: 40 }} onClick={()=>handleMission(index)}>M</Avatar>
-
-                    }
-                  </TableCell>
-                    <TableCell align="center">{row?.title}</TableCell>
-                    <TableCell align="center">{parseInt(row?.reward)} </TableCell>
-                    <TableCell align="center"> - </TableCell>
-                    <TableCell align="center"> XP </TableCell>
-                    <TableCell align="center">{Object.keys(row?.status)[0]}</TableCell>
-                    <TableCell align="center">{"----"}</TableCell>
-                    <TableCell align="center"></TableCell>
-                    <TableCell align="center" onClick={()=>{handleTasks(row)}}><Button
-                      variant="outlined"
-                      sx={{
-                        borderColor: "black", 
-                        color: "black", 
-                        "&:hover": {
-                          backgroundColor: "black", 
-                          color: "white", 
-                          borderColor: "black", 
-                        },
-                      }}
-
+                missionList.map((row, index) => {
+                  console.log("filter : ", row);
+                  return (
+                    <TableRow
+                      key={index}
+                      className="hover:bg-blue-100 cursor-pointer "
                     >
-                      View Tasks
-                    </Button>
-                  </TableCell>
-                  </TableRow>
-                )})
+                      <TableCell
+                        align="center"
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        {row?.img?.length > 0 ? (
+                          <img
+                            src={row?.img[0]}
+                            className="w-[40px] h-[40px] rounded-sm object-fill"
+                            onClick={() => handleMission(index)}
+                          />
+                        ) : (
+                          <Avatar
+                            variant="rounded"
+                            sx={{ width: 40, height: 40 }}
+                            onClick={() => handleMission(index)}
+                          >
+                            M
+                          </Avatar>
+                        )}
+                      </TableCell>
+                      <TableCell align="center">{row?.title}</TableCell>
+                      <TableCell align="center">
+                        {parseInt(row?.reward)}{" "}
+                      </TableCell>
+                      <TableCell align="center"> - </TableCell>
+                      <TableCell align="center"> XP </TableCell>
+                      <TableCell align="center">
+                        {Object.keys(row?.status)[0]}
+                      </TableCell>
+                      <TableCell align="center">{"----"}</TableCell>
+                      <TableCell align="center"></TableCell>
+
+                      <TableCell
+                        align="center"
+                        onClick={() => {
+                          handleTasks(row);
+                        }}
+                      >
+                        <Button
+                          variant="outlined"
+                          sx={{
+                            borderColor: "black",
+                            color: "black",
+                            "&:hover": {
+                              backgroundColor: "black",
+                              color: "white",
+                              borderColor: "black",
+                            },
+                          }}
+                        >
+                          View Tasks
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })
               ) : (
                 <TableRow>
                   <TableCell colSpan={8} align="center">
