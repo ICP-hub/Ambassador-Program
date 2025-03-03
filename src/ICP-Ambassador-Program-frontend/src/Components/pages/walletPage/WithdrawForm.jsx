6b65b99f14d5ca928ac5650634e00f6beab33da5
Rajnish Tripathi <rajnish.tripathi@quadbtech.com>
2025-03-03 19:20:27 +0530

    FEAT: Add balance management to Wallet components and enhance WithdrawForm functionality

--- src/ICP-Ambassador-Program-frontend/src/Components/pages/walletPage/WithdrawForm.jsx
+++ src/ICP-Ambassador-Program-frontend/src/Components/pages/walletPage/WithdrawForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const WithdrawForm = ({ setAmount, setReceiver, withdraw }) => {
+export const WithdrawForm = ({ balance, setAmount, setReceiver, withdraw }) => {
 
   return (
     <section className="flex flex-col items-center mt-6">
@@ -23,27 +23,40 @@ export const WithdrawForm = ({ setAmount, setReceiver, withdraw }) => {
         />
         <div className="flex justify-between px-6 py-4 mt-5 w-full text-xl font-medium rounded-xl bg-[#D9D9D9] bg-opacity-30 max-md:px-5">
           <input
-            type="number"
+            id="amount"
+            type="text"
             placeholder="Enter the amount"
             className="text-white bg-transparent border-none outline-none"
-            onChange={(e) => setAmount(e.target.value)}
+            // value={balance}
+            onChange={(e) => {
+              const value = e.target.value;
+              // Allow only valid floating-point or integer numbers
+              if (/^\d*\.?\d*$/.test(value)) {
+                setAmount(Number(value));
+              }
+            }}
             onWheel={(e) => e.target.blur()}
-
           />
-          <button type="button" className="text-right text-[#9173FF]">
+
+          <button type="button" className="text-right text-[#9173FF]"
+            onClick={() => {
+              setAmount(Number(balance - 0.01));
+              document.getElementById('amount').value = Number(balance - 0.01);
+            }}
+          >
             max
           </button>
         </div>
         <p className="px-5 py-2 mt-7 text-sm text-center text-white font-normal rounded-3xl bg-[#9173FF]/40  w-[165px]">
-          Fee: 0.01 ckUSDC
+          Fee: 0.01 USD
         </p>
         <button
           className="px-14 py-3 mt-7 text-xl font-medium text-white whitespace-nowrap rounded-xl border-2 border-white border-solid w-[187px] max-md:px-5"
-          onClick={withdraw}
+          onClick={(e) => withdraw(e)}
         >
           Confirm
         </button>
       </form>
-</section>
+    </section>
   );
 };
